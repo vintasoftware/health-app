962f5eca3abe262ef32ec523cfca9e87eb08991a
Flávio Juvenal <flavio@vinta.com.br>
2024-12-18 13:34:27 -0300

    Use @medplum/expo-polyfills

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,33 @@
+import { MedplumClient } from "@medplum/core";
+import {
+  ExpoClientStorage,
+  initWebSocketManager,
+  polyfillMedplumWebAPIs,
+} from "@medplum/expo-polyfills";
 import { MedplumProvider } from "@medplum/react-hooks";
-import { ErrorBoundaryProps, Stack, useRouter } from "expo-router";
+import { ErrorBoundaryProps, router, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { useMemo } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-
-import { initMedplumClient } from "@/utils/medplum";
 
 export function ErrorBoundary({ error, _retry }: ErrorBoundaryProps) {
   console.log(error);
   return null;
 }
 
-export default function RootLayout() {
-  const router = useRouter();
-  const medplum = useMemo(
-    () =>
-      initMedplumClient({
-        onUnauthenticated: () => {
-          medplum.clear();
-          router.replace("/sign-in");
-        },
-      }),
-    [router],
-  );
+polyfillMedplumWebAPIs();
+const medplum = new MedplumClient({
+  clientId: process.env.EXPO_PUBLIC_MEDPLUM_CLIENT_ID,
+  storage: new ExpoClientStorage(),
+  onUnauthenticated: () => {
+    medplum.clear();
+    router.replace("/sign-in");
+  },
+});
+initWebSocketManager(medplum);
 
+export default function RootLayout() {
   return (
     <SafeAreaProvider>
       <StatusBar translucent={true} />
