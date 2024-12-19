6078c8c99e6816f8417a8b8ac423d5b1b2cccf57
Flávio Juvenal <flavio@vinta.com.br>
2024-12-19 13:30:12 -0300

    Rename env vars

--- utils/medplum-oauth2.ts
+++ utils/medplum-oauth2.ts
@@ -2,8 +2,8 @@ import { Platform } from "react-native";
 
 export const oauth2ClientId =
   Platform.OS === "web"
-    ? process.env.EXPO_PUBLIC_WEB_MEDPLUM_CLIENT_ID!
-    : process.env.EXPO_PUBLIC_NATIVE_MEDPLUM_CLIENT_ID!;
+    ? process.env.EXPO_PUBLIC_MEDPLUM_WEB_CLIENT_ID!
+    : process.env.EXPO_PUBLIC_MEDPLUM_NATIVE_CLIENT_ID!;
 export const oAuth2Discovery = {
   authorizationEndpoint: "https://api.medplum.com/oauth2/authorize",
   tokenEndpoint: "https://api.medplum.com/oauth2/token",
