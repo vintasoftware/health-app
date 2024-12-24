a6322504b4e03d7b822733f312823286612e1949
Flávio Juvenal <flavio@vinta.com.br>
2024-12-24 08:39:47 -0300

    Improve README explanation about Redirect URL

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,7 @@ import {
   polyfillMedplumWebAPIs,
 } from "@medplum/expo-polyfills";
 import { MedplumProvider } from "@medplum/react-hooks";
+import { makeRedirectUri } from "expo-auth-session";
 import { router, Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
@@ -37,6 +38,13 @@ export default function RootLayout() {
   useEffect(() => {
     // Prevents flickering:
     requestAnimationFrame(SplashScreen.hideAsync);
+  }, []);
+
+  useEffect(() => {
+    // Print redirect URL on startup
+    if (__DEV__) {
+      console.log("Redirect URL:", makeRedirectUri());
+    }
   }, []);
 
   return (
