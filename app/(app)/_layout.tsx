f47ced91b1cec1bd3f794c891236b3d17d7acb03
Flávio Juvenal <flavio@vinta.com.br>
2025-02-07 17:41:15 -0300

    Fix medplum.isLoading issue (https://github.com/medplum/medplum/issues/5933)

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,5 +1,6 @@
+import { MedplumClient } from "@medplum/core";
 import { useMedplumContext } from "@medplum/react-hooks";
-import { Redirect, Slot } from "expo-router";
+import { Redirect, router, Slot } from "expo-router";
 import { useEffect } from "react";
 
 import { LoadingScreen } from "@/components/LoadingScreen";
@@ -7,14 +8,52 @@ import { PractitionerBanner } from "@/components/PractitionerBanner";
 import { ChatProvider } from "@/contexts/ChatContext";
 import { useNotifications } from "@/contexts/NotificationsContext";
 
+async function retryMedplumProfile(medplum: MedplumClient) {
+  // If profile is already loaded, do nothing:
+  if (!medplum.isLoading()) {
+    return;
+  }
+  // If Medplum is not initialized, check again later:
+  if (!medplum.isInitialized) {
+    setTimeout(() => retryMedplumProfile(medplum), 1000);
+    return;
+  }
+
+  // The only way to check if profile fetch failed is to await for getProfileAsync
+  try {
+    await medplum.getProfileAsync();
+  } catch {
+    // If profile fetch failed, force profile reload by calling setActiveLogin,
+    // as it's the only way to clean up the internal profilePromise,
+    // otherwise MedplumClient stays stuck in loading state
+    const activeLogin = medplum.getActiveLogin();
+    if (activeLogin) {
+      medplum.setActiveLogin(activeLogin);
+      // Ensure profile fetch after 1 second:
+      setTimeout(() => retryMedplumProfile(medplum), 1000);
+    } else {
+      // If there's no active login, redirect to sign-in:
+      medplum.clearActiveLogin();
+      router.replace("/sign-in");
+    }
+  }
+}
+
 export default function AppLayout() {
   const { medplum, profile } = useMedplumContext();
   const { setUpPushNotifications } = useNotifications();
   const isPractitioner = profile?.resourceType === "Practitioner";
 
+  // Ensure profile is loaded or loading,
+  // to avoid app getting stuck in medplum.isLoading() state
+  // when an error occurs in the internal profilePromise (due to server or network issues)
+  useEffect(() => {
+    retryMedplumProfile(medplum);
+  }, [medplum]);
+
+  // Set up push notifications when user is logged in
   useEffect(() => {
     if (profile) {
-      // Set up push notifications when user is logged in
       setUpPushNotifications();
     }
   }, [profile, setUpPushNotifications]);
